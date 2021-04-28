4d7fc54efed414acc31ddea8b168998e14e6ed70
tnicacio <nicacio.t92@gmail.com>
2021-04-27 23:04:27 -0300

    Initial StarRating Component

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,13 @@
+import { FaStar } from 'react-icons/fa';
+
+export function StarRating(): JSX.Element {
+  return (
+    <>
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="grey" />
+      <FaStar color="grey" />
+    </>
+  )
+}
