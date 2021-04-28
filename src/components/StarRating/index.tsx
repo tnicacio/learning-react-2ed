240ab03d4d29d7d3a9a0374caad635a1bae41697
tnicacio <nicacio.t92@gmail.com>
2021-04-27 23:30:36 -0300

    useState with constant star number

--- src/components/StarRating/index.tsx
+++ src/components/StarRating/index.tsx
@@ -1,13 +1,19 @@
-import { FaStar } from 'react-icons/fa';
+import { useState } from 'react';
 import { Star } from './Star';
 
 export function StarRating({ totalStars = 5 }): JSX.Element {
+  const [selectedStars] = useState(3);
 
   const createArray = (length: number) => [...Array(length)];
 
   return (
     <>
-      { createArray(totalStars).map((n,i) => <Star key={i}/>) }
+      { createArray(totalStars).map((n,i) => (
+          <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        { selectedStars } of { totalStars } stars
+      </p>
     </>
   )
 }
